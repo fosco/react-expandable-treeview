46ed3f0a80e8a406d5d80f350cae7e19fc280a22
Mirco Bellagamba <mirco.bellag@gmail.com>
2019-10-03 13:28:05 +0200

    Add custom expanded/collapsed icon props

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TreeView from '../lib';
 import testData from './testData';
+import collapsed from './images/right.png';
+import expanded from './images/down.png';
+import nodeIcon from './images/github.png';
 
 export default class App extends React.Component {
   renderNode(node) {
@@ -10,12 +13,33 @@ export default class App extends React.Component {
   render() {
     return (
       <div style={{ padding: 20 }}>
-      <h1>The <i>Felidae</i> family</h1>
-        <TreeView 
-          data={testData}
-          renderNode={this.renderNode}
-          />
+        <h1>The <i>Felidae</i> family</h1>
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: '50%' }}>
+            <h3>Basic sample</h3>
+            <TreeView
+              data={testData}
+              renderNode={this.renderNode}
+            />
+          </div>
+          <div style={{ width: '50%' }}>
+            <h3>Custom sample</h3>
+            <TreeView
+              data={testData}
+              collapsedIcon={collapsed}
+              expandedIcon={expanded}
+              nodeIcon={nodeIcon}
+              nodeSize={20}
+              lineColor="crimson"
+              lineWidth={1}
+              lineAlpha={1}
+              expandButtonColor="transparent"
+              renderNode={this.renderNode}
+            />
+          </div>
+
+        </div>
       </div>
-    )
+    );
   }
 }
